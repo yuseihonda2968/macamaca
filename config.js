cd276e31be4e334ea111275aac6e462e5dc1e6a3
yuseihonda2968 <89965319+yuseihonda2968@users.noreply.github.com>
2026-09-21 17:36:23 +0900

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // ============================================================
 const CONFIG = {
   LIFF_ID: '2011682490-agpxja2j',                                   // LINE Developers > LIFF のID（例: 1234567890-AbCdEfGh）
-  GAS_URL: 'https://liff.line.me/2011682490-agpxja2j'    // GASウェブアプリの /exec URL
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbxsA5Bn3bqikX48rbdzZu8C_guIEHiKXdxqJxR5rKEWVfyA3E8r4wQV20rp9NuC3OzRvw/exec'    // GASウェブアプリの /exec URL
 };
